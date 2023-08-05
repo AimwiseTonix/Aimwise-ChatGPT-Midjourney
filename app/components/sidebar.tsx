b53f0fdfed1ba712c78853a5a605d9dd9d9ebda9
ToniX <aimwisetonix@gmail.com>
2023-08-05 15:17:46 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT-Midjourney</div>
+        <div className={styles["sidebar-title"]}>Aimwise出品</div>
         <div className={styles["sidebar-sub-title"]}>
-          Your own AI assistant.
+          仅供内部使用，请勿外传
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
